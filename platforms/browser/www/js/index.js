ae88bd92d4ab04534010fc167a21f29103b92ba1
Skyler Liu <vcshing2005@hotmail.com>
2016-10-01 19:44:22 +0800

    asd

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -70,14 +70,14 @@ var app = {
 				isTesting: false, // receiving test ads (do not test with real ads as your account will be banned)				// Optional
                 interstitialAdId: admobid.interstitial,
                 bannerAtTop: true, // set to true, to put banner at top
-                overlap: false, // set to true, to allow banner overlap webview
+                overlap: true, // set to true, to allow banner overlap webview
                 offsetStatusBar: true, // set to true to avoid ios7 status bar overlap
               ////  autoShowBanner: true, // auto show banners ad when loaded
                 autoShowInterstitial: true // auto show interstitials ad when loaded
             });
 			
 			// Start showing banners (atomatic when autoShowBanner is set to true)
-				window.admob.createBannerView(); 
+			setTimeout(function(){ 	window.admob.createBannerView(); }, 5000);
 		
 			
         } else {
