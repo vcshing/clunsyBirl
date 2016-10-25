59d8b9077d8c80bab8fab20882020d1fa7c16890
Skyler Liu <vcshing2005@hotmail.com>
2016-10-25 23:23:12 +0800

    sd

--- www/build/clumsy-min.js
+++ www/build/clumsy-min.js
@@ -274,10 +274,10 @@ game.TitleScreen = me.ScreenObject.extend({
     },
     onResetEvent: function() {
 		
-		//if(Math.floor(Math.random() * 3) + 1 == 1 ){
+		if(Math.floor(Math.random() * 4) + 1 == 1 ){
 			
 			window.admob.requestInterstitialAd();
-		//}
+		}
 		if(initadv==0){
 		//	window.admob.createBannerView(function () { }, function (e) {
 				// // alert(JSON.stringify(e));
